b7dc8cffa1fd675e7f2ec841c99015bf9feecf97
aryankoju17 <aryankoju345@gmail.com>
2024-07-01 12:37:55 +0545

    build files

--- CMS/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
+++ CMS/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Additional clean files
 cmake_minimum_required(VERSION 3.16)
 
@@ -9,15 +8,3 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   "CMakeFiles\\CMS_autogen.dir\\ParseCache.txt"
   )
 endif()
-=======
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "CMS_autogen"
-  "CMakeFiles\\CMS_autogen.dir\\AutogenUsed.txt"
-  "CMakeFiles\\CMS_autogen.dir\\ParseCache.txt"
-  )
-endif()
->>>>>>> main
